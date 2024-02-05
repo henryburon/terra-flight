d82536eb8ee951adb6dacc2ea4f843bd422d8908
Henry Buron <henryburon34@gmail.com>
2024-02-05 16:12:39 -0600

    Publishes basic directional messages using ps3 input on robot motion topic

--- terraflight_control/terraflight_control/base_station.py
+++ terraflight_control/terraflight_control/base_station.py
@@ -1,6 +1,7 @@
 import rclpy
 from rclpy.node import Node
 from sensor_msgs.msg import Joy
+from std_msgs.msg import String
 
 
 
@@ -14,17 +15,23 @@ class Base_Station(Node):
         # Timer
         self.timer = self.create_timer(0.01, self.timer_callback) # 100 Hz
 
+        # Publishers
+        self.joy_pub = self.create_publisher(
+            String,
+            'robot_motion',
+            10)
+
         # Subscribers
         self.joy_sub = self.create_subscription(
             Joy,
             'joy',
             self.joy_callback,
             10)
-        
 
         # Variables
         self.ps3_axes = []
         self.ps3_buttons = []
+        self.dpad = [0,0,0,0]
 
 
 
@@ -33,28 +40,54 @@ class Base_Station(Node):
     def timer_callback(self):
         self.get_logger().info("the base station node is up!", once=True)
 
+        self.send_robot_commands()
+
     def joy_callback(self, msg):
         self.get_logger().info("entering joy callback")
+
+        # Collect ps3 controller input values
         self.ps3_axes = msg.axes
         self.ps3_buttons = msg.buttons
 
-        # self.get_logger().info(f'My log message {buttons}')
-
-        # test = buttons[1]
-
-        # self.get_logger().info(f'My log message {test}')
+        # Convert inputs to variables
         self.map_joy_inputs()
-        self.get_logger().info(f"fdjsfdkjsfd {self.left_joystick}")
 
+        # self.get_logger().info(f"Test: {self.left_joystick}")
 
     def map_joy_inputs(self):
-        # Left/Right, Up/Down
-        self.left_joystick = [self.ps3_axes[0],
-                              self.ps3_axes[1]]
+        self.left_joystick = [self.ps3_axes[0], # Left/Right
+                              self.ps3_axes[1]] # Up/Down
         self.right_joystick = [self.ps3_axes[3],
                                self.ps3_axes[4]]
         self.left_trigger = [self.ps3_axes[2]]
         self.right_trigger = [self.ps3_axes[5]]
+
+        # Directional pad
+        self.dpad = [self.ps3_buttons[13], # Top
+                     self.ps3_buttons[16], # Right
+                     self.ps3_buttons[14], # Bottom
+                     self.ps3_buttons[15]] # Left
+
+    def send_robot_commands(self):
+        # Convert input directions to string directions and publish
+        robot_motion = String()
+
+        if all(value == 0 for value in self.dpad):
+            robot_motion.data = "stop"
+        elif self.dpad[0] == 1:
+            robot_motion.data = "forward"
+        elif self.dpad[1] == 1:
+            robot_motion.data = "right"
+        elif self.dpad[2] == 1:
+            robot_motion.data = "backward"
+        elif self.dpad[3] == 1:
+            robot_motion.data = "left"
+        
+        self.joy_pub.publish(robot_motion)
+        
+
+     
+
         
 
 
