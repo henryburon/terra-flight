1905d6812051d58e89cbb56d2975732a84ea659a
Henry Buron <henryburon34@gmail.com>
2024-02-17 20:42:45 -0600

    Current camera node streams when on laptop when i run:
    ros2 run image_view image_view --ros-args --remap /image:=/robot_camera

--- terraflight_control/terraflight_control/fetch_camera.py
+++ terraflight_control/terraflight_control/fetch_camera.py
@@ -2,6 +2,8 @@ import rclpy
 from rclpy.node import Node
 from sensor_msgs.msg import Image
 import pyrealsense2 as rs
+import cv2
+import numpy as np
 
 
 
@@ -18,14 +20,47 @@ class Fetch_Camera(Node):
       # Initialization
 
       # Configure depth and color streams
-      pipeline = rs.pipeline()
-      config = rs.config()
+      self.pipeline = rs.pipeline()
+      self.config = rs.config()
+
+      # Get device product line for setting a supporting resolution
+      self.pipeline_wrapper = rs.pipeline_wrapper(self.pipeline)
+      self.pipeline_profile = self.config.resolve(self.pipeline_wrapper)
+      self.device = self.pipeline_profile.get_device()
+      self.device_product_line = str(self.device.get_info(rs.camera_info.product_line))
+
+      self.config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
+
+      self.config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
+
+      self.pipeline.start(self.config)
+
 
 
 
    # Called at 100 Hz
    def publish_image(self):
-      self.get_logger().info("fsdbfkjsbfksd")
+      frames = self.pipeline.wait_for_frames()
+      color_frame = frames.get_color_frame()
+      if not color_frame:
+         return
+      
+      # Convert images to numpy arrays
+      color_image = np.asanyarray(color_frame.get_data())
+
+      # Create Image message
+      msg = Image()
+      msg.header.stamp = self.get_clock().now().to_msg()
+      msg.header.frame_id = "color_frame"
+      msg.height = color_image.shape[0]
+      msg.width = color_image.shape[1]
+      msg.encoding = 'bgr8' # or bgr8?
+      msg.is_bigendian = False
+      msg.step = color_image.shape[1] * color_image.shape[2]
+      msg.data = color_image.tobytes()
+
+      self.camera_pub.publish(msg)
+      
       
 
 
