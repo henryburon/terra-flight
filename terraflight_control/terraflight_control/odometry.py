b3af9718c5e966098f095e5f8b2969963e9e3388
Henry Buron <henryburon34@gmail.com>
2024-02-23 19:30:23 -0600

    First test for input pins

--- terraflight_control/terraflight_control/odometry.py
+++ terraflight_control/terraflight_control/odometry.py
@@ -1,6 +1,6 @@
 import rclpy
 from rclpy.node import Node
-
+import RPi.GPIO as GPIO
 
 
 class Odometry(Node):
@@ -10,13 +10,25 @@ class Odometry(Node):
       # Timer
       self.timer = self.create_timer(0.01, self.timer_callback) # 100 Hz
 
+      self.a_front_right = 24 # blue
+      self.b_front_right = 23 # white
 
+      # Set up GPIOs
+      GPIO.setmode(GPIO.BCM)
+
+      GPIO.setup(self.a_front_right, GPIO.IN)
+      GPIO.setup(self.b_front_right, GPIO.IN)
 
 
 
    def timer_callback(self):
 
-      self.get_logger().info("fdhsfsdfjs")
+      current_state = GPIO.input(self.a_front_right)
+
+      if current_state == GPIO.HIGH:
+         self.get_logger().info("It is HIGH")
+      elif current_state == GPIO.LOW:
+         self.get_logger().info("It is LOW")
 
 
 
