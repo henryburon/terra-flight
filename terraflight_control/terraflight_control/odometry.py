e4faa2526b28c5b98348284175a14c25bb75298c
Henry Buron <henryburon34@gmail.com>
2024-02-26 16:42:33 -0600

    to 1s

--- terraflight_control/terraflight_control/odometry.py
+++ terraflight_control/terraflight_control/odometry.py
@@ -116,10 +116,10 @@ class Odometry(Node):
 
         self.log_counter = 0
 
-        self.front_left_direction = 0
-        self.front_right_direction = 0
-        self.back_left_direction = 0
-        self.back_right_direction = 0
+        self.front_left_direction = 1 # change these back to zeroes
+        self.front_right_direction = 1
+        self.back_left_direction = 1
+        self.back_right_direction = 1
 
     def timer_callback(self):
         # Main timer callback. Updates the robot configuration.
