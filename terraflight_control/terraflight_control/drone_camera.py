bedf6b6f44a581f5b8ee100a662e21ce746c9be3
Henry Buron <henryburon34@gmail.com>
2024-03-02 21:19:20 -0500

    reorganized camera nodes

--- terraflight_control/terraflight_control/drone_camera.py
+++ terraflight_control/terraflight_control/drone_camera.py
@@ -1,18 +1,16 @@
 import rclpy
 from rclpy.node import Node
 from djitellopy import tello
-import cv2
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge
-
-
+import cv2
 
 class Drone_Camera(Node):
    def __init__(self):
       super().__init__("drone_camera")
 
       # Publishers
-      self.drone_pub = self.create_publisher(Image, "drone_camera", 10)
+      self.drone_pub = self.create_publisher(Image, "/drone_camera", 10)
 
       # Initialize variables for Tello Drone
       self.drone = tello.Tello()
@@ -21,19 +19,13 @@ class Drone_Camera(Node):
       self.bridge = CvBridge()
 
       # Timers
-      self.timer = self.create_timer(1/100, self.timer_callback)
-      
-      # Flags
-      self.start_display_flag = 0
+      self.drone_camera_timer = self.create_timer(1/100, self.drone_image_callback)
 
 
-   def timer_callback(self):
-
+   def drone_image_callback(self):
       # Fetch image from drone
       image = self.drone.get_frame_read()
 
-      # Resize image and make into numpy array. Change fx and fy to increase
-      # the image quality at expense of larger message
       resized = cv2.resize(image.frame, (0,0), fx=0.25, fy=0.25, interpolation=cv2.INTER_AREA)
 
       # Publish image (cite Courtney)
--- terraflight_control/terraflight_control/robot_camera.py
+++ terraflight_control/terraflight_control/robot_camera.py
@@ -1,57 +1,51 @@
 import rclpy
 from rclpy.node import Node
 from sensor_msgs.msg import Image
+import pyrealsense2 as rs
 import numpy as np
 import cv2
-from cv_bridge import CvBridge, CvBridgeError
 
 class Robot_Camera(Node):
-    def __init__(self):
-        super().__init__("robot_camera")
+   def __init__(self):
+      super().__init__("robot_camera")
 
-        # Timers
-        self.camera_timer = self.create_timer(1/5, self.publish_image_callback)
+      # Publishers
+      self.camera_pub = self.create_publisher(Image, "robot_camera", 10)
 
-        # Initialize camera
-        self.cap = cv2.VideoCapture(0)
+      # Timers
+      self.camera_timer = self.create_timer(1/3, self.publish_image)
 
-        # Check if the camera opened successfully
-        if not self.cap.isOpened():
-         print("Error: Could not open external camera.")
-         # If the external camera at index 0 didn't open, try other indices
-         for i in range(1, 10):
-            self.cap = cv2.VideoCapture(i)
-            if self.cap.isOpened():
-                  print(f"External camera found at index {i}")
-                  break
-         else:  # No external camera was found
-            print("No external camera found. Exiting...")
-            exit()
+      # Configure depth and color streams
+      self.pipeline = rs.pipeline()
+      self.config = rs.config()
+      self.config.enable_stream(rs.stream.color, 424, 240, rs.format.bgr8, 6)
+      self.pipeline.start(self.config)
 
-        # Create a publisher
-        self.image_pub = self.create_publisher(Image, 'camera/image', 10)
 
-        # Create a CvBridge object
-        self.bridge = CvBridge()
+   def publish_image(self):
+      frames = self.pipeline.wait_for_frames()
+      color_frame = frames.get_color_frame()
+      if not color_frame:
+         return
 
-    def publish_image_callback(self):
-      self.get_logger().info("Publishing image")
-      ret, frame = self.cap.read()
+      color_image = np.asanyarray(color_frame.get_data())
+      color_image = cv2.resize(color_image, (260, 160))
+      
+      # Create Image message
+      msg = Image()
+      msg.header.stamp = self.get_clock().now().to_msg()
+      msg.header.frame_id = "color_frame"
+      msg.height = color_image.shape[0]
+      msg.width = color_image.shape[1]
+      msg.encoding = 'bgr8'
+      msg.is_bigendian = False
+      msg.step = color_image.shape[1] * color_image.shape[2]
+      msg.data = color_image.tobytes()
 
-      # Resize the image to reduce its quality
-      frame = cv2.resize(frame, (160, 120))  # Change the dimensions as needed
-
-      # Convert the image to a ROS image
-      try:
-         ros_image = self.bridge.cv2_to_imgmsg(frame, "bgr8")
-      except CvBridgeError as e:
-         print(e)
-
-      # Publish the image
-      self.image_pub.publish(ros_image)
-
+      self.camera_pub.publish(msg)
+      
 def robot_camera_entry(args=None):
-    rclpy.init(args=args)
-    node = Robot_Camera()
-    rclpy.spin(node)
-    rclpy.shutdown()
+   rclpy.init(args=args)
+   node = Robot_Camera()
+   rclpy.spin(node)
+   rclpy.shutdown()
