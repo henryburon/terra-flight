d598f9d2dc22480047a12ecb115558e7884e8da1
Henry Buron <henryburon34@gmail.com>
2024-03-05 01:12:36 -0500

    Consistently launches all the nodes correctly, gets video stream, SLAM up, etc.
    Still not completely sure that SLAM is working correctly... but it might be

--- terraflight_description/launch/show_terraflight.launch.py
+++ terraflight_description/launch/show_terraflight.launch.py
@@ -24,7 +24,7 @@ def generate_launch_description():
             #     package="tf2_ros",
             #     executable="static_transform_publisher",
             #     arguments=['0','0','0','0','0','0', 'odom','base_footprint'], # x was at 0.254
-            ),
+            # ),
             Node(
                 package="joint_state_publisher",
                 executable="joint_state_publisher",
